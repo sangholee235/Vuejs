3f2567652c0f487021f46da1bf436068fd10201b
LSH <tkdgh1135@naver.com>
2024-10-24 17:43:01 +0900

    add : modal

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <div>
+      <div class="black-bg" v-if="모달창열렸니 == true">
+        <div class="white-bg">
+          <h4>상세페이지</h4>
+          <p>상세페이지내용임</p>
+          <button @click="모달창열렸니 = false">닫기</button>
+        </div>
+      </div>
+    </div>
+
     <div class="menu">
       <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
     </div>
@@ -7,26 +17,37 @@
     <div>
       <img alt="Vue logo" src="./assets/logo.png" />
       <div v-for="(a, i) in products" :key="i">
-        <h4>{{ products[i] }}</h4>
-        <p>{{ price1 }}만원</p>
-        <button @click="신고수++">허위매물신고</button>
-        <span>신고수 : {{ 신고수 }}</span>
+        <img :src="products[i].image" class="room-img" />
+        <h4 @click="모달창열렸니 = true">{{ products[i].title }}</h4>
+        <p>{{ products[i].price }} 원</p>
+        <!-- <button @click="신고수[i]++">허위매물신고</button>
+        <span>신고수 : {{ 신고수[i] }}</span> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import data from "./assets/oneroom.js";
+
 export default {
   name: "App",
   data() {
     return {
-      신고수: 0,
-      price1: 70,
+      모달창열렸니: false,
+      // 신고수: [0, 0, 0],
+      // price1: 70,
       메뉴들: ["Home", "Shop", "About"],
-      products: ["원룸1", "투룸"],
+      products: data,
     };
   },
+
+  methods: {
+    increase() {
+      this.신고수++;
+    },
+  },
+
   components: {},
 };
 </script>
@@ -49,5 +70,25 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
